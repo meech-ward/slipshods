03990b4835ed54a6669f4af8803bf3775e738909
Sam Meech-Ward <sam@meech-ward.me>
2022-10-11 23:58:09 -0700

    add loading indicator when pushing code

--- src/pages/addPost.jsx
+++ src/pages/addPost.jsx
@@ -7,13 +7,16 @@ import NewPostForm from '../components/NewPostForm'
 import axios from 'axios'
 
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 export default function Profile({ user }) {
   const router = useRouter()
 
+  const [loading, setLoading] = useState(false)
+
   const handleSubmit = async ({ language, code }) => {
+    setLoading(true)
     const { data } = await axios.post('/api/posts', { language, code })
-    console.log(data)
     router.push(`/code/${data.post.id}`)
   }
   return (
@@ -24,9 +27,16 @@ export default function Profile({ user }) {
       <div className="pt-8 pb-10 lg:pt-12 lg:pb-14 max-w-2xl mx-auto px-6 my-6">
         <h1 className='text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl md:text-6xl mb-6'>Create a Snippet</h1>
 
-
         <div className='mt-6'>
-          <NewPostForm className='max-w-2xl' onSubmit={handleSubmit} />
+          {loading ?
+
+            <div className='flex justify-center items-center h-80'>
+              <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-100"></div>
+            </div>
+
+            :
+            <NewPostForm className='max-w-2xl' onSubmit={handleSubmit} />
+          }
         </div>
 
       </div>
