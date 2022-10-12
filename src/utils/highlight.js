4fb5fc9b6629f0cedb64b36129a7c18d6f2dce2a
Sam Meech-Ward <sam@meech-ward.me>
2022-10-11 23:53:22 -0700

    format markdown code blocks

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -1,8 +1,8 @@
 // import hljs from 'highlight.js/lib/core';
 import hljs from 'highlight.js'
 import javascript from 'highlight.js/lib/languages/javascript';
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('js', javascript);
+// hljs.registerLanguage('javascript', javascript);
+// hljs.registerLanguage('js', javascript);
 import markdown from 'highlight.js/lib/languages/markdown';
 hljs.registerLanguage('markdown', markdown);
 
@@ -12,11 +12,13 @@ export default function highlight(code, language) {
   if (language === 'markdown') {
     let highlighted = hljs.highlight(code, languageObject).value
 
+    const blocks = []
     function replacer(match, p1, p2, p3, offset, string) {
-      return "```" + p2 + "" + hljs.highlight(p3, { language: p2 }).value + "```";
+      blocks.push("```" + p2 + "" + hljs.highlight(p3, { language: p2 }).value + "```")
     }
     const regex = /(```(.+?)(?=[\n\r\s])([.\n\r\s\S]*?)(?=`)```)/g
-    highlighted = highlighted.replace(regex, replacer)
+    code.replace(regex, replacer)
+    highlighted = highlighted.replace(regex, () => blocks.shift())
     return highlighted
   }
 
